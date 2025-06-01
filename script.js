f7ea2ef822d8af63cc85f1c377af2cf1327431a5
EASVTECH <easvuy@gmail.com>
2025-05-31 23:16:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 
-fetch('catalogo_productos.json')
-  .then(response => response.json())
-  .then(data => {
-    const contenedor = document.getElementById('catalogo');
-    data.forEach(producto => {
-      const div = document.createElement('div');
-      div.className = 'producto';
-      div.innerHTML = `
-        <h3>${producto.nombre}</h3>
-        <p>${producto.descripcion}</p>
-        <strong>Precio: USD ${producto.precio}</strong>
-      `;
-      contenedor.appendChild(div);
-    });
-  });
